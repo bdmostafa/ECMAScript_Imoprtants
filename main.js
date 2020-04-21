0fc6c40d2985fd341d75fb34f54bc9d6273da4e5
Mostafa <mdmostafaalmahmud@gmail.com>
2020-04-21 12:55:44 +0600

    Spread Operator, push, Rest parameter, Dynamic Function, ES6 local/global variables, Hoisting added

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
+"use strict"; // Strict Mode as like Police for error detection
 console.log("Hello BD!");
+
+myName();
+
+function myName() {
+  var name = "mostafa";
+}
+
+// Spread Operator ==============================
+let IITStudent = ["Mostafa", "Ahmed"];
+
+let IITTeacher = [...IITStudent, "Patuwary", "Jesmin", "Yusuf"];
+
+let IITMembers = [...IITTeacher, ...IITStudent];
+
+// console.log(IITMembers);
+
+// Push method without using spread operator ======================
+let iitStaff = ["Kamal", "Kalam"];
+IITMembers.push(iitStaff);
+
+console.log(IITMembers); // Push function merge in separate array but spread does not
+
+// Rest parameter
+function Summation(...numbers) {
+  let sum = 0;
+  for (let i of numbers) {
+    sum = sum + i;
+  }
+  console.log(sum); // Output is 45
+}
+Summation(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+// Rest parameter + more parameters
+function sumRest(a, b, ...numbers) {
+  let sum = 0;
+  for (let i of numbers) {
+    sum = sum + i;
+  }
+  console.log(sum); // Output is 15 because a and b parameters are not calculated here
+
+  let totalSum = a + b + sum;
+  console.log(totalSum); // Output is 65
+}
+sumRest(20, 30, 1, 2, 3, 4, 5);
+
+// Dynamic function
