ba68ec2acaf4c78b04d95bdce399bbfb39f6a39d
Mostafa <mdmostafaalmahmud@gmail.com>
2020-05-08 22:01:00 +0600

    Generate binray 1 to n using Queue

--- dataStructures/queues.js
+++ dataStructures/queues.js
@@ -45,3 +45,45 @@ console.log(myQueue.printQueue()); // [ 'a', 'b', 'c', 'd', 'e' ]
 console.log(myQueue.dequeue()); // a
 console.log(myQueue.front()); // b
 console.log(myQueue.printQueue()); // [ 'b', 'c', 'd', 'e' ]
+
+
+/* Using Queue constructor, auto generate binary numbers function */
+const generateBinary = function (n) {
+    // Create an object of Queue constructor 
+    var binaryQueue = new Queue();
+
+    // Enqueue the first binary number 0 or 1 from randomStart()
+    binaryQueue.enqueue(randomStart());
+
+    // Random starting function
+    function randomStart() {
+        // Return a random integer from 0 to 1
+        return Math.floor(Math.random() * 2);
+    }
+
+    // Loop until less zero and print binary numbers
+    while (n-- > 0) {
+        // Print the front of queue 
+        var b1 = binaryQueue.front();
+        binaryQueue.dequeue();
+        console.log(b1);
+
+        // Store b1 into b2 before changing it 
+        var b2 = b1;
+
+        // Append "0" to b1 and enqueue it 
+        binaryQueue.enqueue(b1 + "0");
+
+        // Append "1" to b2 and enqueue it
+        binaryQueue.enqueue(b2 + "1");
+
+        // Add toString() to get rid of adding 1 with b1 and b2
+        binaryQueue.enqueue(b1 + randomStart().toString() + "0");
+
+        binaryQueue.enqueue(b2 + randomStart().toString() + "1");
+    }
+}
+
+generateBinary(10);
+// 1 10 11 100 111 100 101 1010 1001 110
+// 0 00 01 010 011 000 001 0010 0011 010
