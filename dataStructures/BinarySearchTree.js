92617cd34fdf7899acdbc80a744cc181102f90ee
Mostafa <mdmostafaalmahmud@gmail.com>
2020-05-09 12:11:27 +0600

    BST - Binary Search Tree started

---
+++ dataStructures/BinarySearchTree.js
@@ -0,0 +1,8 @@
+/*
+Binary Search Tree - BST, a non linear data structure, a collection of node connected by some edges.
+A Binary Search tree is a binary tree in which nodes which have lesser value are stored on the left while the nodes with higher value are stored at the right.
+*/
+
+const BinarySearchTree = function () {
+
+}
